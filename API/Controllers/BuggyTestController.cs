2c225292cdd82f272990813f1326bda87d2357c3
Mario Jackson <mario@jackson.softwre>
2021-12-16 19:23:42 +0100

    Added exception middleware

---
+++ API/Controllers/BuggyTestController.cs
@@ -0,0 +1,41 @@
+using System;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyTestController : BaseApiController
+{
+    [HttpGet("not-found")]
+    public ActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult GetBadRequest()
+    {
+        return BadRequest(new ProblemDetails{Title = "This is a bad request"});
+    }
+
+    [HttpGet("unauthorized")]
+    public ActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("validation-error")]
+    public ActionResult GetValidationError()
+    {
+        ModelState.AddModelError("Problem 1", "This is the first validation error");
+        ModelState.AddModelError("Problem 2", "This is the second validation error");
+
+        return ValidationProblem();
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult GetServerError()
+    {
+        throw new Exception("This is a server error");
+    }
+}
