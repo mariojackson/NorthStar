7bdd5d8ac1296598f022a780acdb701e937b8f74
Mario Jackson <mario@jackson.softwre>
2021-12-17 18:45:05 +0100

    Added basket entity

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
+[Table("BasketItems")]
 public class BasketItem
 {
     public int Id { get; set; }
@@ -7,4 +10,7 @@ public class BasketItem
 
     public int ProductId { get; set; }
     public Product Product { get; set; }
+
+    public int BasketId { get; set; }
+    public Basket Basket { get; set; }
 }
